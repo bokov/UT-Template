b6d04f7d4afefed2caaa0666b002be16b227b7bb
Alex F. Bokov <bokov@uthscsa.edu>
2019-08-20 18:39:30 +0000

    git_setup.R;cleanup;Fewer irrelevant warning messages

--- git_setup.R
+++ git_setup.R
@@ -1,26 +1,30 @@
 # Set up user.name and user.email ----
 
-if(!is.null(attr(system('git config --global user.name',intern=T),'status'))){
+suppressWarnings(if(!is.null(attr(system('git config --global user.name'
+                                         ,intern=T)
+                                  ,'status'))){
   .username <- readline('Please type in the user name you want to use and hit enter: ');
   system(sprintf('git config --global user.name "%s"',.username));
-}
+});
 
-if(!is.null(attr(system('git config --global user.email',intern=T),'status'))){
+suppressWarnings(if(!is.null(attr(system('git config --global user.email'
+                                         ,intern=T)
+                                  ,'status'))){
   .useremail <- readline('Please type in the email address you want to use and hit enter: ');
   system(sprintf('git config --global user.email "%s"',.useremail));
-}
+});
 
 # Set up useful aliases ----
 if(!is.null(attr(suppressWarnings(system('git config alias.upd',intern=T))
                  ,'status'))){
   system('git config --global alias.upd "submodule update --init --recursive\
-         --remote"')} else warning(
+         --remote"')} else message(
            'You already have a git alias named "upd", leaving it unchanged.');
 
 if(!is.null(attr(suppressWarnings(system('git config alias.lg',intern=T))
                  ,'status'))){
   system('git config --global alias.lg "log --oneline --graph --color --all\
-         --decorate"')} else warning(
+         --decorate"')} else message(
            'You already have a git alias named "lg", leaving it unchanged.');
 
 if(!dir.exists('scripts') && dir.exists('../scripts')) setwd('..')
