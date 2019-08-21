0284d3c309ef1f321b0e93bba2b92d167cf366d9
Alex Bokov <bokov@uthscsa.edu>
2019-08-21 17:13:39 -0500

    git_setup.R;cleanup;More robust and general way to check for and replace submodules. Override their pre-emptive deletion by creating a .developer file in the same directory as .gitmodules

--- git_setup.R
+++ git_setup.R
@@ -27,16 +27,27 @@ if(!is.null(attr(suppressWarnings(system('git config alias.lg',intern=T))
          --decorate"')} else message(
            'You already have a git alias named "lg", leaving it unchanged.');
 
-if(!dir.exists('scripts') && dir.exists('../scripts')) setwd('..')
-
-if(dir.exists('scripts')){
-  system('git upd');
-  if(dir.exists('scripts/hooks')) file.copy()
+if(basename(getwd())=='scripts') setwd('..');
+if(!file.exists('.gitmodules')){
+  if(file.exists('../.gitmodules')) setwd('..') else {
+    if(file.exists('../../.gitmodules')) setwd('../..') else {
+      stop('Cannot find the .gitmodules file, cannot update submodules')
+      }}
 }
+
+# delete the current submodule directories and recreate fresh
+# if you are one of the developers for this project, create a file 
+# named .developer in the directory that contains the .gitmodule file
+# otherwise this script will blow away the submodule directories before
+# updating them
+if(!file.exists('.developer')){
+  unlink(system("git submodule --quiet foreach 'echo $path'",intern=T)
+       ,recursive = T,force = T)};
+system('git upd');
+
+#if(dir.exists('scripts/hooks')) file.copy();
 #else warning('
 #  This script should be run in a directory that has a "scripts" subdirectory.
 #  Cannot find that, so not installing git hooks');
-
-
 
 c()
