b75617f36efc817457d67b5fc0dec7ecd365ce0e
Alex Bokov <bokov@uthscsa.edu>
2019-11-21 15:39:20 -0600

    dictionary.R; feature; Trimmed down and revised it to only do renames and 'tblinfo()'

--- dictionary.R
+++ dictionary.R
@@ -16,37 +16,31 @@ if(is.null(.currentscript)) .currentscript <- 'RUN_FROM_INTERACTIVE_SESSION';
 #' Saving original file-list so we don't keep exporting functions and 
 #' environment variables to other scripts
 .origfiles <- ls();
-# read student pre-run script if it exists ----
-#+ echo=FALSE,message=FALSE
-# read dat00 ----
-#' generic read function which auto-guesses file formats:
-#+ echo=F
-# make data dictionary ----
-#' ## Create the data dictionary
-dct0 <- tblinfo(dat00);
-message('Done creating data dictionary, starting variable mapping');
-#' The `varmap.csv` file is for renaming variables. Create
-#' a 'no-change' starter version if one doesn't already exist
-if(!file.exists(file.path(.workdir,'varmap.csv'))){
-  map0 <- tibble::as_tibble(with(dct0,data.frame(origname=column,varname=column
-                                         ,dispname=column,comments=NA
-                                         ,stringsAsFactors = FALSE)));
-  write.csv(map0,file.path(.workdir,'varmap.csv'),row.names = FALSE);
-} else {
-  map0 <- try_import(file.path(.workdir,'varmap.csv'));
-}
-dct0$column <- make.unique(unlist(submulti(dct0$column,map0
-                                           ,method='startsends')));
-names(dat00) <-dct0$column;
-message('Done variable mapping');
-#+ echo=F
-# save out ----
+#+ echo=FALSE
+#### rename_vars ####
+#' Rename variables
+#+ rename_vars
+map0 <- try_import('varmap.csv');
+for(ii in names(rawdata)){
+  map0ii <- subset(map0,table==ii);
+  if(nrow(map0ii)>0){
+    names(rawdata[[ii]]) <- submulti(names(rawdata[[ii]])
+                                     ,map0ii[,c('origname','varname')]
+                                     ,'startsends')};
+};
+#+ echo=FALSE
+#### data_dct ####
+#' Create data dictionaries for all data frames
+#+ data_dct
+for(ii in names(rawdata)) attr(rawdata[[ii]],'dct') <- tblinfo(rawdata[[ii]]);
+#+ echo=FALSE
+#### save out ####
 #' ## Save all the processed data to an rdata file 
 #' 
 #' ...which includes the audit trail
-suppressWarnings(tsave(file=file.path(.workdir
-                                      ,paste0(basename(.currentscript)
-                                              ,'.rdata'))
-                       ,list=setdiff(ls(),.origfiles)));
+suppressWarnings(with(rawdata
+                      ,save(file=file.path(.workdir
+                                           ,paste0(basename(.currentscript)
+                                                   ,'.rdata')),list=ls())));
 #+ echo=F,eval=F
 c()
