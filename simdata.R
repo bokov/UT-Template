e0a3f599383de4741be56e5e9aaf74ff3dd6a434
Alex Bokov <bokov@uthscsa.edu>
2019-12-06 23:50:38 -0600

    simdata.R; debug; More prominent debug messages

--- simdata.R
+++ simdata.R
@@ -17,17 +17,18 @@ if(debug>0) source('global.R') else {
 #' 
 #### simulate data ####
 .origfiles <- ls();
-message('Creating outputsims');
+message('**** Creating outputsims');
 outputsims <- setNames(file.path('data',basename(inputdata)),names(inputdata));
-message('outputsims: ',paste0(outputsims,collapse=','));
+message('**** outputsims: ',paste0(outputsims,collapse=','));
 # remove the ones that are remote links
 outputsims <- outputsims[!grepl('^(ftp|https?)://',outputsims)];
-message('normalized outputsims: ',paste0(normalizePath(file.path(.workdir,outputsims)),collapse=','));
+message('**** normalized outputsims: '
+        ,paste0(normalizePath(file.path(.workdir,outputsims)),collapse=','));
 # remove the ones that already exist in the shared directory
 outputsims <- outputsims[!file.exists(normalizePath(file.path(.workdir
                                                               ,outputsims)
                                                     ,mustWork = FALSE))];
-message('Done creating outputsims');
+message('**** Done creating outputsims');
 simrawdata <- list();
 if(length(outputsims)>0){
   for(ii in names(outputsims)){
